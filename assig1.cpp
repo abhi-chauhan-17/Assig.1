f4da3777a7cfdfafacbce626fc7d9294f24db015
abhi-chauhan-17 <140628843+abhi-chauhan-17@users.noreply.github.com>
2023-07-26 20:48:59 +0530

    assig1.cpp

--- assig1.cpp
+++ assig1.cpp
@@ -14,16 +14,16 @@ int main(){
     char x;								//declaring character type variable to store random alphanumeric value
     x = generateRandomAlphanumeric();				//storing the random alphanumeric character by calling the generateRandomAlphanumeric function
 	
-	printf("The Randomly Generate Character is %c\n\n", x);	//printing the random alphanumeric character that is store in variable x
+    printf("The Randomly Generate Character is %c\n\n", x);	//printing the random alphanumeric character that is store in variable x
 	
 	//try with more variable
-	char y,z,a;
-	y = generateRandomAlphanumeric();
-	z = generateRandomAlphanumeric();						//all the variables have different random alphanumric charactar and it will store in them
-	a = generateRandomAlphanumeric();			
+    char y,z,a;
+    y = generateRandomAlphanumeric();
+    z = generateRandomAlphanumeric();						//all the variables have different random alphanumric charactar and it will store in them
+    a = generateRandomAlphanumeric();			
 	    
     	
-	printf("The Randomly Generate Character is %c\n", y);
+    printf("The Randomly Generate Character is %c\n", y);
     printf("The Randomly Generate Character is %c\n", z);
     printf("The Randomly Generate Character is %c\n", a);	//all the variables print diffrent diffrent random alphanumeric characters that store in them
     
